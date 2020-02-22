b041b457c891c5c6ab2b6a89dff2246b15bc913f
yo-p <papapapipipi42@gmail.com>
2020-02-22 11:37:38 +0000

    yoshiki/root #15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     namespace :customer do
         resources :orders
+        resources :customers
         resources :cart_products, only: [:index, :create, :update, :destroy]
         resources :products, only: [:index, :show]
         resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
