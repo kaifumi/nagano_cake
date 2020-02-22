d205fcc319e20f91bd65747c1a96c06a0dcaac29
yo-p <papapapipipi42@gmail.com>
2020-02-22 11:29:32 +0000

    yoshiki/root #15

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+    namespace :customer do
+        resources :orders
+        resources :cart_products, only: [:index, :create, :update, :destroy]
+        resources :products, only: [:index, :show]
+        resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
+    end
+        
+
+    namespace :admin do
+        resources :products, :genres
+        resources :orders, only: [:index, :show, :update]
+        resources :customers, only: [:index, :show, :edit, :update]
+    end
+
+end
