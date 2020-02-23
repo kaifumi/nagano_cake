e2d778f5a6cc5d8fe9421502f5cd207f750de350
yo-p <papapapipipi42@gmail.com>
2020-02-23 05:25:46 +0000

    yoshiki/add root#15

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
     namespace :customer do
-        resources :orders, only: [:new, :thanks, :show, :create, :index, :purchase]
-        resources :customers, only: [:show, :edit, :update, :destroy_confirm, :destroy]
-        resources :cart_products, only: [:index, :create, :update, :destroy, :destroy_all]
+        resources :orders, only: [:new, :show, :create, :index]
+        resources :customers, only: [:show, :edit, :update,:destroy]
+        resources :cart_products, only: [:index, :create, :update, :destroy]
         resources :products, only: [:index, :show]
         resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
     end
-        
+    get "thanks" => "customers/orders#thanks"
+    delete "/destroy_all" => "customer/cart_products#destroy_all"
+    get "/destroy_confirm" => "customer/customers#destroy_confirm"
+    get "/purchase" => "customer/customers#purchase"
+
 
     namespace :admin do
         root 'admins#top'
-        resources :products, :genres
+        resources :products
+        resources :genres, only: [:index, :create, :edit, :update, :destroy]
         resources :orders, only: [:index, :show, :update]
         resources :customers, only: [:index, :show, :edit, :update]
     end
 
 
-end
+end 
