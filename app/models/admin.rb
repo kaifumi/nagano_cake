fc52d0f31a634178ca785ef14665138b28af416d
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-23 20:49:54 +0900

    admin_model,db#16

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates :created_at, presence: true
+	validates :updated_at, presence: true
+	
 end
