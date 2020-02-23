8b62be89531c3dc30f3d2c96f2c861866748b4fe
kaino <kaifumi1001@gmail.com>
2020-02-23 09:13:59 +0000

    kaino/admin_view_add_text#12

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_121050) do
+ActiveRecord::Schema.define(version: 2020_02_23_053501) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "customer_id"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2020_02_22_121050) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
