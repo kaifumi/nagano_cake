862dba800df12efcbcf20c9e92d3843f1dff25c2
yo-p <papapapipipi42@gmail.com>
2020-02-23 07:40:33 +0000

    root追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -7,19 +7,20 @@ Rails.application.routes.draw do
         resources :products, only: [:index, :show]
         resources :deliveries, only: [:index, :create, :edit, :update, :destroy]
     end
+    root "products#index"
     get "thanks" => "customers/orders#thanks"
     delete "/destroy_all" => "customer/cart_products#destroy_all"
     get "/destroy_confirm" => "customer/customers#destroy_confirm"
-    get "/purchase" => "customer/customers#purchase"
+    get "/purchase" => "customer/orders#purchase"
 
 
     namespace :admin do
-        root 'admins#top'
         resources :products
         resources :genres, only: [:index, :create, :edit, :update, :destroy]
         resources :orders, only: [:index, :show, :update]
         resources :customers, only: [:index, :show, :edit, :update]
     end
+    get "/admin/top" => "admin/admins#top"
 
 
 end 
