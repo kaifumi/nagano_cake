683d8499bcb3f57a9238f894e131b8a096a4b372
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-23 19:01:50 +0900

    customer_model,db_migrate#16

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,17 @@ ActiveRecord::Schema.define(version: 2020_02_23_053501) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "kana_first_name"
+    t.string "kana_last_name"
+    t.string "telephone_number"
+    t.string "postal_code"
+    t.string "address"
+    t.boolean "status", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
