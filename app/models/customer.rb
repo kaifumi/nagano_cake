5dca060ca570e52b8d110563b7b9dcadb8941f5c
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-24 13:09:19 +0900

    custmer_model,db#16

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -41,7 +41,6 @@ class Customer < ApplicationRecord
 
 	validates :postal_code,
 	presence: true,
-	numericality: { only_interger: true },
 	format: {
 		with: /\A[0-9]{3}-[0-9]{4}\z/,
 		message: "〇〇〇-〇〇〇〇のように入力して下さい"
