eb507a4a26ebdb0aa80367f776c81537d118eeb2
yo-p <papapapipipi42@gmail.com>
2020-02-24 09:04:31 +0000

    yoshiki/ジャンルのviewとcontroller#15

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,10 @@ class Genre < ApplicationRecord
 
 	validates :type, presence: true
 	validates :sell_activated, inclusion: { in: [true, false] }
-	validates :created_at, presence: true
-	validates :update_at, presence: true
+	# validates :created_at, presence: true 
+	# validates :updated_at, presence: true
+
+	# attr_accessible :data, :type
+	self.inheritance_column = :_type_disabled
 
 end
