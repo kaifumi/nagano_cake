61aae1114a6459f7c4287fb6d274691e46f085ad
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-24 13:49:52 +0900

    delivery_model,db_migrate#16

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_053501) do
+ActiveRecord::Schema.define(version: 2020_02_23_120503) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "customer_id"
@@ -41,9 +53,29 @@ ActiveRecord::Schema.define(version: 2020_02_23_053501) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "deliveries", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "order_id"
+    t.string "postal_code"
+    t.text "address"
+    t.string "receiver"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "genres", force: :cascade do |t|
     t.string "type"
     t.boolean "sell_activated", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_details", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "purchase_price"
+    t.integer "amount"
+    t.integer "product_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
