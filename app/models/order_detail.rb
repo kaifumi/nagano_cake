dd4d464fe0faf1586966d00bc7c294c00b5b73cc
isuc-76/76 <eriasimoyase@gmail.com>
2020-02-24 13:34:29 +0900

    OrderDetail_model,db#16

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,10 +5,10 @@ class OrderDetail < ApplicationRecord
 
 	validates :product_status, presence: true
 	enum product_status:{
-		着手不可: 0,
-		製作待ち: 1,
-		製作中: 2,
-		製作完了: 3
+		"着手不可": 0,
+		"製作待ち": 1,
+		"製作中": 2,
+		"製作完了": 3
 	}
 
 	validates :created_at, presence: true
