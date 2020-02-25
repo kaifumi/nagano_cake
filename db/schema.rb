a967c1075c6da2e1934b2d6c2f0e6c14b43853b3
yo-p <papapapipipi42@gmail.com>
2020-02-25 08:59:34 +0000

    yoshki/genreのviewいじりました

--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,19 @@ ActiveRecord::Schema.define(version: 2020_02_24_075850) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+
+    t.string "first_name"
+    t.string "last_name"
+    t.string "kana_first_name"
+    t.string "kana_last_name"
+    t.string "telephone_number"
+    t.string "postal_code"
+    t.string "address"
+    t.boolean "status", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
