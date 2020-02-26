a8d0382961154c87f537d04b464a506c50d0391f
kahousami <kaho.usami@gmail.com>
2020-02-26 05:58:56 +0000

    kaho/admin-products#14

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,3 +44,20 @@ $(document).ready(function() {
     hidePrevious: false
   });
 });
+
+//アップロードしたばかりの画像のプレビューを表示する
+$(function() {
+ function readURL(input) {
+   if (input.files && input.files[0]) {
+     var reader = new FileReader();
+
+     reader.onload = function (e) {
+       $('#image_prev').attr('src', e.target.result);
+     }
+     reader.readAsDataURL(input.files[0]);
+   }
+ }
+ $("#products_image").change(function(){
+   readURL(this);
+ });
+});
--- db/schema.rb
+++ db/schema.rb
@@ -38,25 +38,9 @@ ActiveRecord::Schema.define(version: 2020_02_24_075850) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.string "first_name"
-    t.string "last_name"
-    t.string "kana_first_name"
-    t.string "kana_last_name"
-    t.string "telephone_number"
-    t.string "postal_code"
-    t.string "address"
-    t.boolean "status", default: true
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-<<<<<<< HEAD
-=======
     t.datetime "deleted_at"
-    
->>>>>>> 46bbc655ef7cec439491dfd1795c6b62e4b1c2a3
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
