0415697a8109dc2df8f0a7e6b0f20c8e1dfaca1a
kahousami <kaho.usami@gmail.com>
2020-02-27 08:56:16 +0000

    kaho/product#14

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -7,4 +7,10 @@ class Customer::ProductsController < ApplicationController
   def show
   	@Product = Product.find(parames[:id])
   end
+
+  private
+  def products_params
+  	params.require(:product).permit(:name, :content, :image_id, :price, :selling_status)
+  end
+
 end
