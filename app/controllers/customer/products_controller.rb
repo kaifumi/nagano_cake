b0ebdc75b0d8eef8899d23adb9bc655ee921c531
kaino <kaifumi1001@gmail.com>
2020-02-29 01:56:15 +0000

    kaino/review#12

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,8 +1,12 @@
 class Customer::ProductsController < ApplicationController
   def index
     
+    
   end
 
   def show
+
+    @product=Product.find(params[:id])
+    @reviews=Review.where(product_id:params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_075850) do
+ActiveRecord::Schema.define(version: 2020_02_26_115834) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,10 +38,6 @@ ActiveRecord::Schema.define(version: 2020_02_24_075850) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
     t.string "first_name"
     t.string "last_name"
     t.string "kana_first_name"
@@ -53,7 +49,6 @@ ActiveRecord::Schema.define(version: 2020_02_24_075850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
-    
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
@@ -108,6 +103,16 @@ ActiveRecord::Schema.define(version: 2020_02_24_075850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.float "rate"
+    t.string "title"
+    t.text "content"
+    t.integer "product_id"
+    t.integer "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
