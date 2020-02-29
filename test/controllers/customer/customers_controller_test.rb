b4edd1690f7c8a9db2eeee5f395d2e44dc66a0db
kaino <kaifumi1001@gmail.com>
2020-02-29 07:40:12 +0000

    kaino/one_test_ok#12

--- test/controllers/customer/customers_controller_test.rb
+++ test/controllers/customer/customers_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class Customer::CustomersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+# class Customer::CustomersControllerTest < ActionController::TestCase
+  include Devise::Test::IntegrationHelpers
+  
+  def setup
+    @customer=customers(:syokaturyo)
+    sign_in(@customer)
+  end
+
+  test "should get customer show" do
+    get customer_customer_path(@customer)
+    assert_response :success
+  end
+
+  test "should get customer edit" do
+    get edit_customer_customer_path(@customer)
+    assert_response :success
+  end
+
 end
