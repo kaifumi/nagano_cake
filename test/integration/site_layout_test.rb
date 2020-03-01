d1f49334001e381c4e153c6fe82828518a50a24c
kaino <kaifumi1001@gmail.com>
2020-03-01 08:17:08 +0000

    kaino/test_add#12

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @customer=customers(:syokaturyo)
+  end
+  
   test "layout links when not logged in" do
+    sign_in(@customer)
     get root_path
+    assert_template "/"
+    assert_select "a[href=?]",root_path
+    assert_select "a[href=?]",customer_customer_path(@customer.id)
+    assert_select "a[href=?]",customer_cart_products_path
+    assert_select "a[href=?]",destroy_customer_session_path
   end
 end
